8994f7190f25f00e69b276946a11c0054a7e5ad3
leoli <cvsf04456@yahoo.com.tw>
2016-07-26 20:30:20 +0800

    update mediator

--- DesignPatternTest/DesignPatternTest.Test/UnitMediator.cs
+++ DesignPatternTest/DesignPatternTest.Test/UnitMediator.cs
@@ -9,6 +9,21 @@ namespace DesignPatternTest.Test
         [TestMethod]
         public void TestMethodMediator()
         {
+            ConcreteMediator m = new ConcreteMediator();
+
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+        }
+
+        [TestMethod]
+        public void TestMethodMediator_Example()
+        {
             UnitedNationsSecurityCouncil UNSC = new UnitedNationsSecurityCouncil();
             TW c1 = new TW(UNSC);
             JP c2 = new JP(UNSC);
