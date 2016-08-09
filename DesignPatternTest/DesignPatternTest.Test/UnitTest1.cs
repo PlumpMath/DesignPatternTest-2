a3f04e6857510f623257335244f49fa1f5566f54
leoli <cvsf04456@yahoo.com.tw>
2016-08-09 20:37:40 +0800

    add Interpreter

---
+++ DesignPatternTest/DesignPatternTest.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace DesignPatternTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_Interpreter()
+        {
+            InterpretContext context = new InterpretContext();
+            ArrayList list = new ArrayList();
+            
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            // Interpret
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+        }
+    }
+}
