830fed3631f40ba20435e6548a17eb95f8542428
leoli <cvsf04456@yahoo.com.tw>
2016-08-16 19:43:10 +0800

    add Memento

---
+++ DesignPatternTest/DesignPatternTest.Common/Memento/Caretaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternTest
+{
+    public class Caretaker
+    {
+        private Memento _memento;
+
+        // Gets or sets memento
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
---
+++ DesignPatternTest/DesignPatternTest.Test/UnitTestMemento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternTest.Test
+{
+    [TestClass]
+    public class UnitTestMemento
+    {
+        [TestMethod]
+        public void TestMethod_Memento()
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            // Store internal state
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            // Continue changing originator
+            o.State = "Off";
+
+            // Restore saved state
+            o.SetMemento(c.Memento);
+        }
+    }
+}
